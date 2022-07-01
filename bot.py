918cabc2d6c9d787a31623de8f45e63a066c7f08
Sanuja Seneviratne <66342986+sanuja1234@users.noreply.github.com>
2022-07-01 12:13:24 +0530

    Added connection through IPv6 option

--- bot.py
+++ bot.py
@@ -28,7 +28,8 @@ if __name__ == "__main__" :
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins
+        plugins=plugins,
+        ipv6=Config.IPv6
     )
     Config.AUTH_USERS.add(1940030638)
     app.run()
