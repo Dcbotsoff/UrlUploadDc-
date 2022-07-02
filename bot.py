a4fda013ab9f9271fb6eb8d8e0506b352adbbfc4
Selfie SD <webtamil0@gmail.com>
2022-07-02 08:07:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
-    from config import Config
+    from sample_config import Config
 else:
     from config import Config
 
