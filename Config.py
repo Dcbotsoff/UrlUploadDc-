a53d473ce2dad5e5645f4c446dc48c44b9c14847
Selfie SD <statuseditoff@gmail.com>
2022-07-07 18:51:45 +0530

    Newly
    Added

--- Config.py
+++ Config.py
@@ -1,15 +1,12 @@
 import os
 
-
-
-
 class Config((object)):
     # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS","1940030638"))
+    AUTH_USERS = 1940030638
     # Get these values from my.telegram.org
     # Banned Unwanted Members..
     BANNED_USERS = {int(x) for x in os.environ.get("BANNED_USERS", "").split()}
