28f47f07a99e7c530a72edeef004eafcc20fc392
Selfie SD <webtamil0@gmail.com>
2022-07-07 21:26:30 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -25,7 +25,7 @@ from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboa
 
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["help"]))
-async def help_user(bot, message, update):
+async def help_user(bot, update, message):
         # logger.info(update)
         await bot.send_message(
             chat_id=update.chat.id,
@@ -37,7 +37,7 @@ async def help_user(bot, message, update):
 
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
-async def start(bot, message, update):
+async def start(bot, update, message):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.START_TEXT.format(update.from_user.first_name),
