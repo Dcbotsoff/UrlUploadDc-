b3a16fa261da8154895c8591f39f1dff14f362b7
Selfie SD <webtamil0@gmail.com>
2022-07-07 20:32:38 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,21 +1,23 @@
 class Translation(object):
     START_TEXT = """ʜɪ {},
 ɪ ᴀᴍ ᴜʀʟ ᴜᴘʟᴏᴀᴅᴇʀ ᴅᴄ ʙᴏᴛ!
+
 ʏᴏᴜ ᴄᴀɴ ᴜᴘʟᴏᴀᴅ ʜᴛᴛᴘ/ʜᴛᴛᴘꜱ ᴅɪʀᴇᴄᴛ ʟɪɴᴋ, ᴜꜱɪɴɢ ᴛʜɪꜱ ʙᴏᴛ!
+
 /help ꜰᴏʀ ᴍᴏʀᴇ ᴅᴇᴛᴀɪʟꜱ!"""
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail. ⚡"
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
-    DOWNLOAD_START = "Now Downloading.."
-    UPLOAD_START = "Now Uploading.."
+    DOWNLOAD_START = "Now Downloading.....🔥"
+    UPLOAD_START = "Now Uploading....⚡"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @GroupDcBots)"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@xurluploaderbot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @GroupDcBots ⚡"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "⚡ Downloaded in {} seconds.\n⚡ Uploaded in {} seconds.\n\n 🚸 Powered by @GroupDc"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     CUSTOM_CAPTION_UL_FILE = "{}"
     NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
-    HELP_USER = """How to Use Me? Follow These steps!
+    HELP_USER = """How to Use Me? Follow These steps! ⚠️
     
 1. Send url (example.domain/File.mp4 | New Filename.mp4).
 2. Send Image As Custom Thumbnail (Optional).
@@ -25,9 +27,11 @@ URL | filename | username | password"""
    Video  - Give File as video without Screenshots
    File   - Give File without Screenshots
 
-If bot didn't respond, contact @selfiebd"""
+If bot didn't respond, contact @selfiebd 🙃
+ 
+🚸 Powered by @GroupDc"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /genthumbnail to a media album, to generate custom thumbnail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
     CANCEL_STR = "Process Cancelled"
-    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds ⚡"
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
