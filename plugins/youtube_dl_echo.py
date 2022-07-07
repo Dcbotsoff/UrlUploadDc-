56d9cc429a2f0787ab50f88862e0e19d0b0f279d
Selfie SD <webtamil0@gmail.com>
2022-07-07 21:44:44 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -201,7 +201,7 @@ async def echo(bot, update):
             await bot.send_message(
                 chat_id=update.chat.id,
                 text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
-                reply_to_message_id=update.message_id,
+                
                 parse_mode="html",
                 disable_web_page_preview=True
             )
